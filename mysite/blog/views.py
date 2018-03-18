2e33d56629f9152c7d2e8b2ff90d10ef42665b81
kamlesh023 <kamleshpatil921@gmail.com>
2018-03-18 16:49:54 +0530

    "we added get list"

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+        return render(request,'blog/post_list.html',{})
+
+def get_list(request):
+        return render(request,'blog/get_list.html',{})
