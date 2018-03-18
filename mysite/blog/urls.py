d1a533fbaa0389f8ec5511e2f14e850d2b988a9f
kamlesh023 <kamleshpatil921@gmail.com>
2018-03-18 17:14:22 +0530

    three labels

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^abc/$', views.get_list, name='get_list'),
+    url(r'abc/$', views.get_list, name='get_list'),
 ]
